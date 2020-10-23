256c22cd30c7deb028bbebd749cee0282505edc8
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-23 17:34:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,8 +95,6 @@ class TicTacToe
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
         return combo
-      else
-        false
       end
     end
   end
